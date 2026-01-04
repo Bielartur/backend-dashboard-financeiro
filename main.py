08f161269ebb893a5fe1ede163680f4abce47d4e
Gabriel Canto <gabrielcanto2016@gmail.com>
2026-01-04 20:29:57 -0300

    Adicionando o CORS para o frontend React

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.database.core import engine, Base
-from src.entities.payment import Payment # Import models to register them
-from src.entities.category import Category # Import models to register them
-from src.entities.user import User # Import models to register them
+from src.entities.payment import Payment  # Import models to register them
+from src.entities.category import Category  # Import models to register them
+from src.entities.user import User  # Import models to register them
 from src.api import register_routes
 
 from src.logging import configure_logging, LogLevels
@@ -10,6 +11,14 @@ from src.logging import configure_logging, LogLevels
 configure_logging(LogLevels.info)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 """ Only uncomment below to create new tables,
 otherwise the tests will fail if not connected
